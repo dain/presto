adf1c7c479ce675e2b18301fcafef8325b413ccc
Dain Sundstrom <dain@iq80.com>
2021-07-17 13:11:11 -0700

    Add checkRoleExists helper method

--- core/trino-main/src/main/java/io/trino/metadata/MetadataUtil.java
+++ core/trino-main/src/main/java/io/trino/metadata/MetadataUtil.java
@@ -37,6 +37,7 @@ import java.util.Optional;
 import static com.google.common.base.Preconditions.checkArgument;
 import static io.trino.spi.StandardErrorCode.MISSING_CATALOG_NAME;
 import static io.trino.spi.StandardErrorCode.MISSING_SCHEMA_NAME;
+import static io.trino.spi.StandardErrorCode.ROLE_NOT_FOUND;
 import static io.trino.spi.StandardErrorCode.SYNTAX_ERROR;
 import static io.trino.spi.security.PrincipalType.ROLE;
 import static io.trino.spi.security.PrincipalType.USER;
@@ -200,6 +201,20 @@ public final class MetadataUtil
         }
         QualifiedObjectName name = new QualifiedObjectName(session.getCatalog().get(), session.getSchema().get(), table);
         return metadata.getTableHandle(session, name).isPresent();
+    }
+
+    public static void checkRoleExists(Session session, Node node, Metadata metadata, String role, String catalog)
+    {
+        if (!metadata.roleExists(session, role, catalog)) {
+            throw semanticException(ROLE_NOT_FOUND, node, "Role '%s' does not exist in catalog '%s'", role, catalog);
+        }
+    }
+
+    public static void checkRoleExists(Session session, Node node, Metadata metadata, TrinoPrincipal principal, String catalog)
+    {
+        if (principal.getType() == ROLE) {
+            checkRoleExists(session, node, metadata, principal.getName(), catalog);
+        }
     }
 
     public static class TableMetadataBuilder
